c4565b55692be9cd6ea029bb6ee5f4ab9da0d7c4
Maaz123R <maazrakhange39@gmail.com>
2026-08-02 16:38:48 +0530

    Replace BotMessage with debug version

--- frontend/src/components/BotMessage.jsx
+++ frontend/src/components/BotMessage.jsx
@@ -1,7 +1,4 @@
-import ResponseRouter from "./ResponseRouter";
 import TextBubble from "./renderers/TextBubble";
-import { useContext } from "react";
-import { LangContext } from "../context/LangContext";
 
 export default function BotMessage({ content, functionName, args }) {
   console.log("BotMessage:", { content, functionName, args });
@@ -27,46 +24,3 @@ export default function BotMessage({ content, functionName, args }) {
     </div>
   );
 }
-  // Other function responses (cards, tables, etc.)
-  if (functionName) {
-    return (
-      <div
-        style={{
-          alignSelf: "flex-start",
-          maxWidth: "90%",
-          background: "#ffffff",
-          borderRadius: 16,
-          padding: 16,
-          boxShadow: "0 2px 10px rgba(0,0,0,.08)",
-          marginBottom: 10,
-        }}
-      >
-        <ResponseRouter
-          functionName={functionName}
-          args={args}
-          lang={lang}
-        />
-      </div>
-    );
-  }
-
-  // Plain text responses
-  return (
-    <div
-      style={{
-        alignSelf: "flex-start",
-        maxWidth: "90%",
-        background: "#ffffff",
-        borderRadius: 16,
-        padding: 16,
-        boxShadow: "0 2px 10px rgba(0,0,0,.08)",
-        color: "#111827",
-        marginBottom: 10,
-      }}
-    >
-      <TextBubble
-        data={{ message: content }}
-        lang={lang}
-      />
-    </div>
-  );
